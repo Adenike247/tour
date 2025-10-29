25a72da0bfd0ad7db20b0790e3f76437fe995437
Adenike <barakah24777@gmail.com>
2025-10-29 11:39:21 +0100

    add loading svg

--- src/tour/Loading.tsx
+++ src/tour/Loading.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+// src/tour/Loading.tsx
+import Image from 'next/image';
+import React from 'react';
 
 const Loading = () => {
   return (
-    <main>
-           <div className="text-xl">Loading...</div>
-
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="text-center">
+        <Image 
+          src="/tube-spinner.svg" 
+          alt="Loading..." 
+          width={120} 
+          height={120}
+          className="mx-auto mb-4"
+        />
+        <div className="text-xl text-gray-600">Loading tours...</div>
+      </div>
     </main>
+  );
+};
 
-  )
-}
-
-export default Loading
+export default Loading;
